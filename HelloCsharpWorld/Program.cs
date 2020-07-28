9c6a7f7a0c99e119e5eefe55f8ffb839d8adfec1
John Fleming <johndeclanfleming@gmail.com>
2020-07-27 20:31:33 -0500

    Add project files.

---
+++ HelloCsharpWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCsharpWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
