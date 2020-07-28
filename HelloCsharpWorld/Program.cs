293f26cb1cfc1c5f38a9b532fcb0241340d193cc
John Fleming <johndeclanfleming@gmail.com>
2020-07-27 21:39:01 -0500

    simple user input

--- HelloCsharpWorld/Program.cs
+++ HelloCsharpWorld/Program.cs
@@ -6,7 +6,38 @@ namespace HelloCsharpWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var southern = "Howdy Y'all!!";
+            var spanish = "Hola Amigos!!";
+            var japanese = "Konnichiwa!!";
+            var greeting = "Pick a Greeting: Southern, Spanish, or Japanese";
+
+            Console.WriteLine(greeting);
+            var userGreeting = Console.ReadLine();
+
+            if (userGreeting == "Spanish")
+            {
+                Console.WriteLine(spanish);
+            }
+            else if (userGreeting == "Japanese")
+            {
+                Console.WriteLine(japanese);
+            }
+            else
+            {
+                Console.WriteLine(southern);
+            }
+
+            Console.WriteLine("Here are some animals that start with the letter 'T'");
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+
+            foreach (var animal in animals)
+            {
+                if (animal[0] == 'T')
+                {
+                    Console.WriteLine(animal);
+                }
+            }
         }
     }
 }
